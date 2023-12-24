d8e217ddcc402d5c9e661f16bacf4c99c361fa70
Wagner Celso Josué <wjosue@unilavras.edu.br>
2023-12-24 11:05:46 -0300

    Ajustes, inclusão e efeitos e fotos

--- js/main.js
+++ js/main.js
@@ -54,4 +54,92 @@ faqItems.forEach(item => {
         const iconElement = faqBtn.querySelector('i');
         iconElement.classList = `${iconClass} text-2xl`;
     });
-}
+});
+
+//scroll reveal animations
+const sr = ScrollReveal({
+    origin: 'bottom',
+    distance: '60px',
+    duration: 3000,
+    delay: 500,
+});
+
+//hero
+sr.reveal('.hero__text', {origin: 'top'});
+sr.reveal('.hero__img');
+
+//stats
+sr.reveal('.stats__item', {
+    delay: 500,
+    distance: '100px',
+    interval: 100,
+    origin: 'top'
+});
+
+//services
+sr.reveal('.services');
+sr.reveal('.services__top');
+sr.reveal('.services__item',{
+    delay: 500,
+    distance: '100px',
+    interval: 100,
+    origin: 'bottom'
+});
+
+//appoinments
+sr.reveal('.appointment__title');
+sr.reveal('.calendly-inline-widget');
+
+//testimonials
+sr.reveal('.testimonial');
+sr.reveal('.testimonial__constainer');
+
+//team
+sr.reveal('.team__title',{
+    origin: 'left',
+});
+sr.reveal('.team__slider',{
+    origin: 'left',
+});
+
+//faq
+sr.reveal('.faq__title');
+sr.reveal('.faq__item', {
+    delay: 500,
+    distance: '100px',
+    interval: 100,
+    origin: 'bottom'
+});
+
+//departaments
+sr.reveal('.departaments__bg');
+sr.reveal('.departaments__container');
+
+//blog
+sr.reveal('.blog__title');
+sr.reveal('.blog__post', {
+    delay: 500,
+    distance: '100px',
+    interval: 100,
+    origin: 'bottom'
+});
+
+//brands
+sr.reveal('.brands__logo', {
+    delay: 500,
+    distance: '100px',
+    interval: 100,
+    origin: 'bottom'
+});
+
+sr.reveal('.newsletter');
+sr.reveal('.newsletter__container');
+
+
+//footer
+sr.reveal('.footer__item', {
+    delay: 500,
+    distance: '100px',
+    interval: 100,
+    origin: 'bottom'
+});
